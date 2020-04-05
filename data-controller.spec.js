e2466090e09eff80cedfa8f60ef6d556af1d992f
Luiz Henrique Canet Filho <me@luizca.net>
2020-04-05 15:09:51 -0300

    Tests setup

---
+++ data-controller.spec.js
@@ -0,0 +1,12 @@
+/* global describe, it */
+import { expect } from 'chai'
+import DataController from './data-controller'
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const { window } = new JSDOM()
+const { document } = window
+
+describe('Data Controller', function () {
+  it('Expect to get a new Data Controller', function () {
+  })
+})
