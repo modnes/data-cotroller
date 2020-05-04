924ec7ffc7a4c01a02ea5238f273d0a4c87913b5
Luiz Henrique Canet Filho <me@luizca.net>
2020-05-04 17:19:29 -0300

    Using REST API to get last release of package

--- modules/package-download/package-download.js
+++ modules/package-download/package-download.js
@@ -1,38 +1,15 @@
 /* global HTMLElement, fetch */
 export default class PackageDownload extends HTMLElement {
   async connectedCallback () {
-    const RESPONSE = await fetch('https://api.github.com/graphql', {
-      method: 'POST',
-      headers: {
-        Authorization: 'bearer 2d3f821517c05ea92bd72da66356be4933cad497'
-      },
-      body: JSON.stringify({
-        query: `
-          {
-            repository(owner:"${this.getAttribute('owner')}", name:"${this.getAttribute('repository')}") {
-              releases(last: 1) {
-                edges {
-                  node {
-                    tag {
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `
-      })
-    })
+    const RESPONSE = await fetch(`https://api.github.com/repos/${this.getAttribute('owner') || ''}/${this.getAttribute('repository')}/releases/latest`)
     const DATA = await RESPONSE.json()
-    const VERSION_TAG = DATA.data.repository.releases.edges.shift().node.tag.name
 
     this.insertAdjacentHTML('afterBegin', `
-      <a href="https://github.com/${this.getAttribute('owner') || ''}/${this.getAttribute('repository')}/archive/${VERSION_TAG}.zip">
-        ${this.getAttribute('repository')}-${VERSION_TAG}.zip
+      <a href="https://github.com/${this.getAttribute('owner') || ''}/${this.getAttribute('repository')}/archive/${DATA.tag_name}.zip">
+        ${this.getAttribute('repository')}-${DATA.tag_name}.zip
       </a> |
-      <a href="https://github.com/${this.getAttribute('owner') || ''}/${this.getAttribute('repository')}/archive/${VERSION_TAG}.tar.gz">
-        ${this.getAttribute('repository')}-${VERSION_TAG}.tar.gz
+      <a href="https://github.com/${this.getAttribute('owner') || ''}/${this.getAttribute('repository')}/archive/${DATA.tag_name}.tar.gz">
+        ${this.getAttribute('repository')}-${DATA.tag_name}.tar.gz
       </a>
     `)
   }
